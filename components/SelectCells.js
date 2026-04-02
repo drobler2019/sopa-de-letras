d4a13ac83f70cda122b420bf8c17ef9821e8a842
drobler2019 <drobler2019@gmail.com>
2026-04-01 19:50:05 -0500

    Agregar estilos a celdas seleccionadas diagonalmente

--- components/SelectCells.js
+++ components/SelectCells.js
@@ -1,3 +1,5 @@
+import { EventManager } from "../events/EventManager.js";
+
 let firstElement = null;
 let wordSearch = null;
 const values = {};
@@ -18,7 +20,7 @@ const getParagraphElement = (element) => {
   };
 };
 
-const selectFirstCell = (target) => (firstElement = target);
+const selectFirstCell = (event) => (firstElement = event.target);
 
 const getTdElement = (query) => {
   const td = wordSearch.querySelector(query);
@@ -65,30 +67,67 @@ const selectColumn = () => {
   }
 };
 
-const select = (target) => {
-  const {
-    trElement: trFirst,
-    tdElement: tdFirst,
-    paragraph: pFirst,
-  } = getParagraphElement(firstElement);
+const addStylesDiagonalFalling = (attribute, diagonal) => {
+  let count = values.rowFirst;
+  while (count <= values.rowLast) {
+    const query = `tr:nth-child(${count}) td[${attribute}='${diagonal}']`;
+    getTdElement(query);
+    count++;
+  }
+};
 
-  const {
-    trElement: trLast,
-    tdElement: tdLast,
-    paragraph: pLast,
-  } = getParagraphElement(target);
+const addStylesDiagonalAscendant = (attribute, diagonal) => {
+  let count = values.rowFirst;
+  while (count >= values.rowLast) {
+    const query = `tr:nth-child(${count}) td[${attribute}='${diagonal}']`;
+    getTdElement(query);
+    count--;
+  }
+};
+
+const selectDiagonal = () => {
+  if (values.rowFirst < values.rowLast) {
+    if (values.diagonalLeftFirst === values.diagonalLeftLast) {
+      addStylesDiagonalFalling("diagonalleft", values.diagonalLeftLast);
+    } else if (values.diagonalRightFirst === values.diagonalRightLast) {
+      addStylesDiagonalFalling("diagonalright", values.diagonalRightLast);
+    }
+  } else {
+    if (values.diagonalLeftFirst === values.diagonalLeftLast) {
+      addStylesDiagonalAscendant("diagonalleft", values.diagonalLeftLast);
+    } else if (values.diagonalRightFirst === values.diagonalRightLast) {
+      addStylesDiagonalAscendant("diagonalright", values.diagonalRightLast);
+    }
+  }
+};
+
+const select = (event) => {
+  const { trElement: trFirst, tdElement: tdFirst } =
+    getParagraphElement(firstElement);
+
+  const { trElement: trLast, tdElement: tdLast } = getParagraphElement(
+    event.target,
+  );
 
   values.rowFirst = Number.parseInt(trFirst.getAttribute("row"));
   values.rowLast = Number.parseInt(trLast.getAttribute("row"));
   values.columnFirst = Number.parseInt(tdFirst.getAttribute("column"));
   values.columnLast = Number.parseInt(tdLast.getAttribute("column"));
+  values.diagonalLeftFirst = Number.parseInt(tdFirst.getAttribute("diagonalleft"));
+  values.diagonalRightFirst = Number.parseInt(tdFirst.getAttribute("diagonalright"));
+  values.diagonalLeftLast = Number.parseInt(tdLast.getAttribute("diagonalleft"));
+  values.diagonalRightLast = Number.parseInt(tdLast.getAttribute("diagonalright"));
 
   selectRow();
   selectColumn();
+  selectDiagonal();
 };
 
 export const selectCells = (table) => {
   wordSearch = table;
-  table.addEventListener("mousedown", ({ target }) => selectFirstCell(target));
-  table.addEventListener("mouseup", ({ target }) => select(target));
+  const event = new EventManager(
+    [table, table],
+    ["mousedown", "mouseup"],
+    [selectFirstCell, select],
+  );
 };
